954d1b7c2b36c2e91907461917cda1a6aeb15f62
fillateo <jaitramadandij@gmail.com>
2020-08-19 07:57:58 +0800

    Test daftar_jurusan_view

--- app_bpp/tests/test_view_daftar_jurusan.py
+++ app_bpp/tests/test_view_daftar_jurusan.py
@@ -6,7 +6,7 @@ from ..models import Jurusan
 from ..forms import JurusanForm
 
 
-class TestDaftarJurusan(TestCase):
+class DaftarJurusanTest(TestCase):
     def setUp(self):
         self.username = 'fillateo'
         self.password = 'F1ll4t30'
@@ -17,3 +17,34 @@ class TestDaftarJurusan(TestCase):
 
     def test_success_status_code(self):
         self.assertEqual(self.response.status_code, 200)
+
+    def test_resolve(self):
+        self.assertEqual(resolve(self.url).func, list_jurusan_view)
+
+    def test_contains_link_to_daftar_jurusan(self):
+        self.assertContains(self.response, 'href="{}"'.format(self.url))
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, JurusanForm)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_inputs(self):
+        self.assertContains(self.response, '<input', 2)
+
+    def test_contain_button(self):
+        self.assertContains(self.response, 'type="submit"', 1)
+
+    def test_contain_list_of_jurusan(self):
+        daftar_jurusan = self.response.context.get('daftar_jurusan')
+        self.assertTrue(daftar_jurusan.count() == 0)
+
+
+class LoginRequiredDaftarJurusanTest(TestCase):
+    def test_redirection(self):
+        url = reverse('app_bpp:daftar_jurusan')
+        login_url = reverse('accounts:login')
+        response = self.client.get(url)
+        self.assertRedirects(response, "{}?next={}".format(login_url, url))
